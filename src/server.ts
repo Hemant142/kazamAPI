41f3dace15163037fb1d22cf862f13c289f20ebf
Hemant <121333203+Hemant142@users.noreply.github.com>
2025-04-28 12:46:45 +0530

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 import { Server as SocketIOServer } from "socket.io";
 import app from "./app";
-const PORT = process.env.PORT || 4000;
+const port = process.env.PORT || 4000;
 import connection from "./Config/db";
 import { setupSocket } from "./Sockets";
 const server = http.createServer(app);
@@ -15,7 +15,7 @@ const io = new SocketIOServer(server, {
 setupSocket(io);
 
 
-server.listen(PORT, async () => {
+server.listen(port, async () => {
   connection();
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${port}`);
 });
