1af32c4340b557c4c89fcbcbffb2584317f1e4eb
Hemant142 <yhemant203@gmail.com>
2025-04-28 10:14:16 +0530

    install node module

--- dist/app.js
+++ dist/app.js
@@ -1,9 +1,7 @@
 "use strict";
-var __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const cors_1 = __importDefault(require("cors"));
 const express_1 = __importDefault(require("express"));
@@ -15,6 +13,6 @@ app.use(express_1.default.json());
 app.use((0, cors_1.default)());
 app.use("/tasks", tasks_1.default);
 app.get("/", (req, res) => {
-  res.status(200).send(`Welcome to the KAZAM Backend`);
+    res.status(200).send(`Welcome to the KAZAM Backend`);
 });
 exports.default = app;
--- dist/server.js
+++ dist/server.js
@@ -1,42 +1,16 @@
 "use strict";
-var __awaiter =
-  (this && this.__awaiter) ||
-  function (thisArg, _arguments, P, generator) {
-    function adopt(value) {
-      return value instanceof P
-        ? value
-        : new P(function (resolve) {
-            resolve(value);
-          });
-    }
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
-      function fulfilled(value) {
-        try {
-          step(generator.next(value));
-        } catch (e) {
-          reject(e);
-        }
-      }
-      function rejected(value) {
-        try {
-          step(generator["throw"](value));
-        } catch (e) {
-          reject(e);
-        }
-      }
-      function step(result) {
-        result.done
-          ? resolve(result.value)
-          : adopt(result.value).then(fulfilled, rejected);
-      }
-      step((generator = generator.apply(thisArg, _arguments || [])).next());
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
-  };
-var __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const http_1 = __importDefault(require("http"));
 const socket_io_1 = require("socket.io");
@@ -47,15 +21,13 @@ const Sockets_1 = require("./Sockets");
 (0, dotenv_1.configDotenv)();
 const server = http_1.default.createServer(app_1.default);
 const io = new socket_io_1.Server(server, {
-  cors: {
-    origin: "*",
-  },
+    cors: {
+        origin: "*",
+    },
 });
 (0, Sockets_1.setupSocket)(io);
 const PORT = process.env.PORT || 9890;
-server.listen(PORT, () =>
-  __awaiter(void 0, void 0, void 0, function* () {
+server.listen(PORT, () => __awaiter(void 0, void 0, void 0, function* () {
     (0, db_1.default)();
     console.log(`Server is running on port ${PORT}`);
-  })
-);
+}));
